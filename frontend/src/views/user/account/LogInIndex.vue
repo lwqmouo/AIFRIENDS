4092184572e1f877cb0d598995f02cf5c0a4b39c
lwqmouo <3092508782@qq.com>
2026-02-02 11:43:16 +0800

    实现注册 登录页面

--- frontend/src/views/user/account/LogInIndex.vue
+++ frontend/src/views/user/account/LogInIndex.vue
@@ -3,7 +3,22 @@
 </script>
 
 <template>
-login
+  <div class="flex justify-center mt-33" >
+    <fieldset class="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+      <label class="label">Username</label>
+      <input type="text" class="input" placeholder="Enter your username" />
+
+      <label class="label">Password</label>
+      <input type="password" class="input" placeholder="Enter your password" />
+
+      <button class="btn btn-neutral mt-4">Log in</button>
+      <div class="flex justify-end">
+        <RouterLink :to="{name:'user-account-register-index'}" class="btn btn-sm btn-ghost">
+          Sign up
+        </RouterLink>
+      </div>
+    </fieldset>
+  </div>
 </template>
 
 <style scoped>
--- frontend/src/views/user/account/RegisterIndex.vue
+++ frontend/src/views/user/account/RegisterIndex.vue
@@ -3,7 +3,25 @@
 </script>
 
 <template>
-Register
+  <div class="flex justify-center mt-33" >
+    <fieldset class="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+      <label class="label">Username</label>
+      <input type="text" class="input" placeholder="Enter your username" />
+
+      <label class="label">Password</label>
+      <input type="password" class="input" placeholder="Enter your password" />
+
+      <label class="label">Confirm Password</label>
+      <input type="password" class="input" placeholder="Confirm your password" />
+
+      <button class="btn btn-neutral mt-4">Sign up</button>
+      <div class="flex justify-end">
+        <RouterLink :to="{name:'user-account-login-index'}" class="btn btn-sm btn-ghost">
+          Log in
+        </RouterLink>
+      </div>
+    </fieldset>
+  </div>
 </template>
 
 <style scoped>
